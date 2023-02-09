6384c7050ce6b068c3d5a88885dd1f1463cecaab
Bill Garrett <garrett@astralfrontier.org>
2023-02-09 03:21:15 -0800

    Add a demo hello function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,19 @@ interface DefaultLayoutProps {
     children?: React.ReactNode
 }
 
+async function sayHello() {
+    const response = await fetch("/.netlify/functions/hello")
+    const data = await response.json()
+    alert(data.message)
+}
+
 export default function HomePage(props: DefaultLayoutProps) {
     return (
         <>
             <DefaultLayout>
                 <h1 className="title">Sentinels of the Multiverse</h1>
+
+                <button className="button is-primary" onClick={sayHello}>Say Hello</button>
             </DefaultLayout>
         </>
     )
