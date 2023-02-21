fc724028b139cac0b59b5d5164d6bcb703708494
Bill Garrett <garrett@astralfrontier.org>
2023-02-21 13:45:36 -0800

    Add cards to digital JSON

--- src/components/SentinelsDataJson.tsx
+++ src/components/SentinelsDataJson.tsx
@@ -1,8 +1,12 @@
-import { find } from "ramda";
-import React from "react";
+import camelCase from 'camelcase';
+import { find, join, map, partition, split, startsWith } from "ramda";
 
 import { Card, DeckData, Setup } from "../../netlify/functions/notion-retrieve";
 import { SentinelsDataDisplayProps } from "./SentinelsData";
+
+function identifier(input: string): string {
+  return camelCase(input.replace(/[^a-zA-Z0-9]+/g, ''), {pascalCase: true})
+}
 
 function findPrimarySetupCard(setup: Setup[]): Setup {
   for (let card of setup) {
@@ -17,20 +21,41 @@ function findPrimarySetupCard(setup: Setup[]): Setup {
   return setup[0]
 }
 
+function flavor(quote_text: string): any {
+  const [flavorReferences, flavorQuotes] = partition(startsWith("@"), split("\n", quote_text))
+  return {
+    flavorQuotes: map(quote => ({identifier: "Unknown", text: quote}), flavorQuotes),
+    flavorReference: join('', flavorReferences).substring(1)
+  }
+}
+
 function cardsToJson(deckData: DeckData) {
-  return []
+  return map(card => {
+    const { flavorQuotes, flavorReference } = flavor(card.quote_text)
+    return {
+      identifier: identifier(card.name),
+      count: card.quantity,
+      title: card.name,
+      keywords: card.keywords,
+      icons: [], // TODO
+      body: split("\n", card.effects),
+      flavorQuotes,
+      flavorReference
+    }
+  }, deckData.cards)
 }
 
 function deckDataToJson(deckData: DeckData): any {
   const primarySetupCard = findPrimarySetupCard(deckData.setup)
-  const identifier = `${primarySetupCard.name.replace(/\s+/g, '')}Character`
+  const cardName = identifier(`${primarySetupCard.name} Character`)
   const output: any = {
     name: primarySetupCard.name,
     kind: find(tag => (tag == "Hero" || tag == "Villain" || tag == "Environment"), primarySetupCard.tags),
     expansionIdentifier: "TODO",
     initialCardIdentifiers: [
-      identifier
+      cardName
     ],
+    // TODO: add character card
     cards: cardsToJson(deckData),
     promoCards: []
   }
