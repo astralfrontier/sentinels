0d1bf0fec6864770a99a96ee56d4bdab16b58f9a
Bill Garrett <garrett@astralfrontier.org>
2023-03-08 00:48:28 -0800

    Explicitly specify REDIRECT_URI

--- src/components/AuthorizationButton.tsx
+++ src/components/AuthorizationButton.tsx
@@ -8,7 +8,7 @@ const NOTION_CLIENT_ID = import.meta.env.NOTION_CLIENT_ID
 const AUTHORIZATION_URL: string = "https://api.notion.com/v1/oauth/authorize"
 
 export function authorizationUrl(): string {
-  const baseUrl = import.meta.env.DEPLOY_PRIME_URL
+  const baseUrl = import.meta.env.VITE_REDIRECT_URI
   const redirect_uri = new URL("/oauth2/notion", baseUrl).toString()
   return `${AUTHORIZATION_URL}?client_id=${encodeURIComponent(NOTION_CLIENT_ID)}&response_type=code&owner=user&redirect_uri=${encodeURIComponent(redirect_uri)}`
 }
